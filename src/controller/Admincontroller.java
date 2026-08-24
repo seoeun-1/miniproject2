e75228e80e6eb1e9e4e1dae3e1ac74b50c13b671
admin <gmgd055@gmail.com>
2026-08-24 17:01:48 +0900

    admincontoller 수정

--- src/controller/Admincontroller.java
+++ src/controller/Admincontroller.java
@@ -21,8 +21,8 @@ public class Admincontroller {
     }
 
     //상품별 판매 이력 조회
-    public ArrayList<ManagementDto> findAllSale(int pno) {
-        ArrayList<ManagementDto> result = ad.findAllSale(pno);
+    public ArrayList<ManagementDto> findAllout(int pno) {
+        ArrayList<ManagementDto> result = ad.findAllout(pno);
         return result;
     }
 
