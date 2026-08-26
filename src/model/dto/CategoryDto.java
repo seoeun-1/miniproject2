a67b751217a407a7871b8d1377364b242659e017
yurin0109 <yurin0109>
2026-08-26 18:11:39 +0900

    0826_feat 새로운 기능 추가(유린)

--- src/model/dto/CategoryDto.java
+++ src/model/dto/CategoryDto.java
@@ -1,37 +1,28 @@
 package model.dto;
 
-public class CategoryDto {   // 카테고리 테이블의 정보를 담을 객체
-
-    private int cno;        // 카테고리 번호 (PK)
-    private String cname;   // 카테고리 이름
+public class CategoryDto {
+    private int cno;         // 카테고리 번호 (PK)
+    private String cname;    // 카테고리명
 
     // 1. 기본 생성자
     public CategoryDto() {}
 
-    // 2. 등록용 생성자 (cno는 DB에서 자동증가할 때 사용)
-    public CategoryDto(String cname) {
-        this.cname = cname;
-    }
-
-    // 3. 전체 필드 생성자 (조회/수정용)
+    // 2. 매개변수 생성자
     public CategoryDto(int cno, String cname) {
         this.cno = cno;
         this.cname = cname;
     }
 
-    // Getter & Setter
+    // 3. Getter & Setter
     public int getCno() { return cno; }
     public void setCno(int cno) { this.cno = cno; }
 
     public String getCname() { return cname; }
     public void setCname(String cname) { this.cname = cname; }
 
-    // 디버깅 및 데이터 확인용 toString()
+    // 4. 출력용 toString (콘솔에 찍힐 때 보기 편하게)
     @Override
     public String toString() {
-        return "CategoryDto{" +
-                "cno=" + cno +
-                ", cname='" + cname + '\'' +
-                '}';
+        return cno + ". " + cname;
     }
 }
