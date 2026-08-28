52cfb858a07055cf9f3e6819696a32a2a3eaa58c
JUN <guswns020607@naver.com>
2026-08-28 12:27:32 +0900

    08-28 feat: 마감 할인 적용상품

--- src/model/dto/DiscountPriceDto.java
+++ src/model/dto/DiscountPriceDto.java
@@ -3,10 +3,25 @@ package model.dto;
 import java.time.LocalDate;
 
 public class DiscountPriceDto extends BaseDiscountPriceDto {
-    String pname;
-    int originPrice;
-    int mno;
+    private String pname;
+    private int originPrice;
+    private int mno;
 
+    public String getPname() { return pname; }
+    public void setPname(String pname) { this.pname = pname; }
+
+    public int getOriginPrice() { return originPrice; }
+    public void setOriginPrice(int originPrice) {
+        this.originPrice = originPrice;
+    }
+
+    public DiscountPriceDto(int pprice, LocalDate mdate) {
+        super(pprice, mdate);
+    }
+
+
+    public int getMno() { return mno; }
+    public void setMno(int mno) { this.mno = mno; }
     
     
 }
